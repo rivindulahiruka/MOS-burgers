f01fe35bfc07c156e73dc9dae37b8a749af3cfae
Rivindu Lahiruka <rivindulahiruka08@gmail.com>
2024-10-05 00:16:36 +0530

    first commit

--- order.js
+++ order.js
@@ -1,4 +1,82 @@
+const allProducts = [
+  { itemCode: "B1001", itemName: "Classic Burger (Large)", image: "img/OIP.jpeg", price: "750.00", discount: 0, Qty: 50, category: "Burgers" },
+  { itemCode: "B1002", itemName: "Classic Burger (Regular)", image: "img/OIP.jpeg", price: "1500.00", discount: "15%", Qty: 50, category: "Burgers" },
+  { itemCode: "B1003", itemName: "Turkey Burger", image: "img/OIP.jpeg", price: "1600.00", discount: 0, Qty: 50, category: "Burgers" },
+  { itemCode: "B1004", itemName: "Chicken Burger (Large)", image: "img/OIP.jpeg", price: "1400.00", discount: 0, Qty: 50, category: "Burgers" },
+  { itemCode: "B1005", itemName: "Chicken Burger (Regular)", image: "img/OIP.jpeg", price: "800.00", discount: "20%", Qty: 50, category: "Burgers" },
+  
+  // Drinks
+  { itemCode: "B1044", itemName: "Pepsi (330ml)",image :"img/vecteezy_ai-generated-a-glass-of-cola-with-ice-on-a-transparent_37796599.png", price: "990.00", discount: "5%", Qty: 50, category: "Beverages" },
+  { itemCode: "B1045", itemName: "Coca-Cola (330ml)",image :"img/vecteezy_ai-generated-a-glass-of-cola-with-ice-on-a-transparent_37796599.png", price: "1230.00", discount: 0, Qty: 50, category: "Beverages" },
+  { itemCode: "B1046", itemName: "Sprite (330ml)",image :"img/vecteezy_ai-generated-a-glass-of-cola-with-ice-on-a-transparent_37796599.png", price: "1500.00", discount: "3%", Qty: 50, category: "Beverages" },
+  { itemCode: "B1047", itemName: "Mirinda (330ml)",image :"img/vecteezy_ai-generated-a-glass-of-cola-with-ice-on-a-transparent_37796599.png", price: "850.00", discount: "7%", Qty: 50, category: "Beverages" },
 
+  // // Chicken Items
+  // { itemCode: "B1038", itemName: "Fried Chicken (Small)", price: "1200.00", discount: 0, Qty: 50, category: "Chicken" },
+  // { itemCode: "B1039", itemName: "Fried Chicken (Regular)", price: "2300.00", discount: "10%", Qty: 50, category: "Chicken" },
+  // { itemCode: "B1040", itemName: "Fried Chicken (Large)", price: "3100.00", discount: "5%", Qty: 50, category: "Chicken" },
+  // { itemCode: "B1041", itemName: "Hot Wings (Large)", price: "2400.00", discount: 0, Qty: 50, category: "Chicken" },
+
+  // Pasta
+  { itemCode: "B1031", itemName: "Chicken n Cheese Pasta",image:"img/vecteezy_spaghetti-italian-pasta-with-tomato-sauce_9887173.png", price: "1600.00", discount: "15%", Qty: 50, category: "Pasta" },
+  { itemCode: "B1032", itemName: "Chicken Penne Pasta",image:"img/vecteezy_spaghetti-italian-pasta-with-tomato-sauce_9887173.png", price: "1700.00", discount: 0, Qty: 50, category: "Pasta" },
+  { itemCode: "B1033", itemName: "Ground Turkey Pasta Bake",image:"img/vecteezy_spaghetti-italian-pasta-with-tomato-sauce_9887173.png", price: "2900.00", discount: "10%", Qty: 50, category: "Pasta" },
+  { itemCode: "B1034", itemName: "Creamy Shrimp Pasta",image:"img/vecteezy_spaghetti-italian-pasta-with-tomato-sauce_9887173.png", price: "2000.00", discount: 0, Qty: 50, category: "Pasta" },
+  { itemCode: "B1035", itemName: "Lemon Butter Pasta", image:"img/vecteezy_spaghetti-italian-pasta-with-tomato-sauce_9887173.png",price: "1950.00", discount: 0, Qty: 50, category: "Pasta" },
+  { itemCode: "B1036", itemName: "Tagliatelle Pasta",image:"img/vecteezy_spaghetti-italian-pasta-with-tomato-sauce_9887173.png", price: "2400.00", discount: "1%", Qty: 50, category: "Pasta" },
+  { itemCode: "B1037", itemName: "Baked Ravioli",image:"img/vecteezy_spaghetti-italian-pasta-with-tomato-sauce_9887173.png", price: "2000.00", discount: "1%", Qty: 50, category: "Pasta" },
+
+  // Fries
+  { itemCode: "B1025", itemName: "Steak Fries (Large)",image :"img/fries-6.png", price: "1200.00", discount: 0, Qty: 50, category: "Fries" },
+  { itemCode: "B1026", itemName: "Steak Fries (Medium)",image :"img/fries-6.png", price: "600.00", discount: 0, Qty: 50, category: "Fries" },
+  { itemCode: "B1027", itemName: "French Fries (Large)",image :"img/fries-6.png", price: "800.00", discount: 0, Qty: 50, category: "Fries" },
+  { itemCode: "B1028", itemName: "French Fries (Medium)",image :"img/fries-6.png", price: "650.00", discount: 0, Qty: 50, category: "Fries" },
+  { itemCode: "B1029", itemName: "French Fries (Small)",image :"img/fries-6.png", price: "450.00", discount: 0, Qty: 50, category: "Fries" },
+  { itemCode: "B1030", itemName: "Sweet Potato Fries (Large)",image :"img/fries-6.png", price: "600.00", discount: 0, Qty: 50, category: "Fries" },
+
+  // Submarines
+  { itemCode: "B1020", itemName: "Grinder Submarine",image:"img/sandwich-transparent-14 (1).png", price: "2300.00", discount: 0, Qty: 50, category: "Submarines" },
+  { itemCode: "B1021", itemName: "Cheese Submarine",image:"img/sandwich-transparent-14 (1).png", price: "2300.00", price: "2200.00", discount: 0, Qty: 50, category: "Submarines" },
+  { itemCode: "B1022", itemName: "Double Cheese n Chicken Submarine",image:"img/sandwich-transparent-14 (1).png", price: "2300.00", price: "1900.00", discount: "16%", Qty: 50, category: "Submarines" },
+  { itemCode: "B1023", itemName: "Special Horgie Submarine",image:"img/sandwich-transparent-14 (1).png", price: "2300.00", price: "2800.00", discount: 0, Qty: 50, category: "Submarines" },
+  { itemCode: "B1024", itemName: "MOS Special Submarine",image:"img/sandwich-transparent-14 (1).png", price: "2300.00", price: "3000.00", discount: 0, Qty: 50, category: "Submarines" }
+];
+
+
+if (!localStorage.getItem('allProducts')) {
+  localStorage.setItem('allProducts', JSON.stringify(allProducts));
+}
+
+
+function displayTable(category) {
+  let productData = JSON.parse(localStorage.getItem('allProducts')) || [];
+
+ 
+  if (category !== 'All') {
+      productData = productData.filter(product => product.category === category);
+  }
+
+  const tableBody = document.getElementById("burgerTableBody");
+  tableBody.innerHTML = ""; 
+
+  
+  productData.forEach((product, index) => {
+      const row = tableBody.insertRow();
+      row.insertCell(0).textContent = product.itemCode;
+      row.insertCell(1).textContent = product.itemName;
+      row.insertCell(2).textContent = product.price;
+      row.insertCell(3).textContent = product.Qty;
+      row.insertCell(4).textContent = product.discount === 0 ? '-' : product.discount;
+
+      const editCell = row.insertCell(5);
+      editCell.innerHTML = `<button class="edit-button" >Edit</button>`;
+      const deleteCell = row.insertCell(6);
+      deleteCell.innerHTML = `<button class="delete-button" onclick="deleteProduct(${index})">Delete</button>`;
+  });
+}
+
+
+displayTable('All');
 let selectedProducts = JSON.parse(localStorage.getItem('selectedProducts')) || [];
 
 // Global order details
@@ -239,100 +317,42 @@ function generatePDF() {
 
 
 
-const allProducts = [
-  { itemCode: "B1001", itemName: "Classic Burger (Large)", image: "img/OIP.jpeg", price: "750.00", discount: 0, Qty: 50, category: "Burgers" },
-  { itemCode: "B1002", itemName: "Classic Burger (Regular)", image: "img/OIP.jpeg", price: "1500.00", discount: "15%", Qty: 50, category: "Burgers" },
-  { itemCode: "B1003", itemName: "Turkey Burger", image: "img/OIP.jpeg", price: "1600.00", discount: 0, Qty: 50, category: "Burgers" },
-  { itemCode: "B1004", itemName: "Chicken Burger (Large)", image: "img/OIP.jpeg", price: "1400.00", discount: 0, Qty: 50, category: "Burgers" },
-  { itemCode: "B1005", itemName: "Chicken Burger (Regular)", image: "img/OIP.jpeg", price: "800.00", discount: "20%", Qty: 50, category: "Burgers" },
-  
-  // Drinks
-  { itemCode: "B1044", itemName: "Pepsi (330ml)",image :"img/vecteezy_ai-generated-a-glass-of-cola-with-ice-on-a-transparent_37796599.png", price: "990.00", discount: "5%", Qty: 50, category: "Beverages" },
-  { itemCode: "B1045", itemName: "Coca-Cola (330ml)",image :"img/vecteezy_ai-generated-a-glass-of-cola-with-ice-on-a-transparent_37796599.png", price: "1230.00", discount: 0, Qty: 50, category: "Beverages" },
-  { itemCode: "B1046", itemName: "Sprite (330ml)",image :"img/vecteezy_ai-generated-a-glass-of-cola-with-ice-on-a-transparent_37796599.png", price: "1500.00", discount: "3%", Qty: 50, category: "Beverages" },
-  { itemCode: "B1047", itemName: "Mirinda (330ml)",image :"img/vecteezy_ai-generated-a-glass-of-cola-with-ice-on-a-transparent_37796599.png", price: "850.00", discount: "7%", Qty: 50, category: "Beverages" },
 
-  // // Chicken Items
-  // { itemCode: "B1038", itemName: "Fried Chicken (Small)", price: "1200.00", discount: 0, Qty: 50, category: "Chicken" },
-  // { itemCode: "B1039", itemName: "Fried Chicken (Regular)", price: "2300.00", discount: "10%", Qty: 50, category: "Chicken" },
-  // { itemCode: "B1040", itemName: "Fried Chicken (Large)", price: "3100.00", discount: "5%", Qty: 50, category: "Chicken" },
-  // { itemCode: "B1041", itemName: "Hot Wings (Large)", price: "2400.00", discount: 0, Qty: 50, category: "Chicken" },
+function addItem() {
+  const itemCode = document.getElementById("product-code").value;
+  const itemName = document.getElementById("product-name").value;
+  const price = document.getElementById("product-price").value;
+  const discount = document.getElementById("product-discount").value;
+  const category = document.getElementById("categories").value;
 
-  // Pasta
-  { itemCode: "B1031", itemName: "Chicken n Cheese Pasta",image:"img/vecteezy_spaghetti-italian-pasta-with-tomato-sauce_9887173.png", price: "1600.00", discount: "15%", Qty: 50, category: "Pasta" },
-  { itemCode: "B1032", itemName: "Chicken Penne Pasta",image:"img/vecteezy_spaghetti-italian-pasta-with-tomato-sauce_9887173.png", price: "1700.00", discount: 0, Qty: 50, category: "Pasta" },
-  { itemCode: "B1033", itemName: "Ground Turkey Pasta Bake",image:"img/vecteezy_spaghetti-italian-pasta-with-tomato-sauce_9887173.png", price: "2900.00", discount: "10%", Qty: 50, category: "Pasta" },
-  { itemCode: "B1034", itemName: "Creamy Shrimp Pasta",image:"img/vecteezy_spaghetti-italian-pasta-with-tomato-sauce_9887173.png", price: "2000.00", discount: 0, Qty: 50, category: "Pasta" },
-  { itemCode: "B1035", itemName: "Lemon Butter Pasta", image:"img/vecteezy_spaghetti-italian-pasta-with-tomato-sauce_9887173.png",price: "1950.00", discount: 0, Qty: 50, category: "Pasta" },
-  { itemCode: "B1036", itemName: "Tagliatelle Pasta",image:"img/vecteezy_spaghetti-italian-pasta-with-tomato-sauce_9887173.png", price: "2400.00", discount: "1%", Qty: 50, category: "Pasta" },
-  { itemCode: "B1037", itemName: "Baked Ravioli",image:"img/vecteezy_spaghetti-italian-pasta-with-tomato-sauce_9887173.png", price: "2000.00", discount: "1%", Qty: 50, category: "Pasta" },
+  const allProducts = JSON.parse(localStorage.getItem('allProducts')) || [];
+  let image;
 
-  // Fries
-  { itemCode: "B1025", itemName: "Steak Fries (Large)",image :"img/fries-6.png", price: "1200.00", discount: 0, Qty: 50, category: "Fries" },
-  { itemCode: "B1026", itemName: "Steak Fries (Medium)",image :"img/fries-6.png", price: "600.00", discount: 0, Qty: 50, category: "Fries" },
-  { itemCode: "B1027", itemName: "French Fries (Large)",image :"img/fries-6.png", price: "800.00", discount: 0, Qty: 50, category: "Fries" },
-  { itemCode: "B1028", itemName: "French Fries (Medium)",image :"img/fries-6.png", price: "650.00", discount: 0, Qty: 50, category: "Fries" },
-  { itemCode: "B1029", itemName: "French Fries (Small)",image :"img/fries-6.png", price: "450.00", discount: 0, Qty: 50, category: "Fries" },
-  { itemCode: "B1030", itemName: "Sweet Potato Fries (Large)",image :"img/fries-6.png", price: "600.00", discount: 0, Qty: 50, category: "Fries" },
-
-  // Submarines
-  { itemCode: "B1020", itemName: "Grinder Submarine",image:"img/sandwich-transparent-14 (1).png", price: "2300.00", discount: 0, Qty: 50, category: "Submarines" },
-  { itemCode: "B1021", itemName: "Cheese Submarine",image:"img/sandwich-transparent-14 (1).png", price: "2300.00", price: "2200.00", discount: 0, Qty: 50, category: "Submarines" },
-  { itemCode: "B1022", itemName: "Double Cheese n Chicken Submarine",image:"img/sandwich-transparent-14 (1).png", price: "2300.00", price: "1900.00", discount: "16%", Qty: 50, category: "Submarines" },
-  { itemCode: "B1023", itemName: "Special Horgie Submarine",image:"img/sandwich-transparent-14 (1).png", price: "2300.00", price: "2800.00", discount: 0, Qty: 50, category: "Submarines" },
-  { itemCode: "B1024", itemName: "MOS Special Submarine",image:"img/sandwich-transparent-14 (1).png", price: "2300.00", price: "3000.00", discount: 0, Qty: 50, category: "Submarines" }
-];
-
-
-if (!localStorage.getItem('allProducts')) {
-  localStorage.setItem('allProducts', JSON.stringify(allProducts));
-}
-
-
-function displayTable(category) {
-  let productData = JSON.parse(localStorage.getItem('allProducts')) || [];
-
- 
-  if (category !== 'All') {
-      productData = productData.filter(product => product.category === category);
+  // Assign image based on category
+  if (category === 'Burgers') {
+      image = 'img/OIP.jpeg';
+  } else if (category === 'Beverages') {
+      image = 'img/vecteezy_ai-generated-a-glass-of-cola-with-ice-on-a-transparent_37796599.png'; 
+  } else if (category === 'Pasta') {
+      image = 'img/vecteezy_spaghetti-italian-pasta-with-tomato-sauce_9887173.png'; 
+  } else if (category === 'Fries') {
+      image = 'img/fries-6.png'; 
+  } else if (category === 'Submarines') {
+      image = 'img/sandwich-transparent-14 (1).png'; 
+  } else {
+      image = 'img/default-image.jpg'; 
   }
 
-  const tableBody = document.getElementById("burgerTableBody");
-  tableBody.innerHTML = ""; 
+  const newItem = { itemCode, itemName, image, price, discount: discount || "-", category };
+  allProducts.push(newItem);
+  localStorage.setItem('allProducts', JSON.stringify(allProducts));
 
-  
-  productData.forEach((product, index) => {
-      const row = tableBody.insertRow();
-      row.insertCell(0).textContent = product.itemCode;
-      row.insertCell(1).textContent = product.itemName;
-      row.insertCell(2).textContent = product.price;
-      row.insertCell(3).textContent = product.Qty;
-      row.insertCell(4).textContent = product.discount === 0 ? '-' : product.discount;
-
-      const editCell = row.insertCell(5);
-      editCell.innerHTML = `<button class="edit-button" >Edit</button>`;
-      const deleteCell = row.insertCell(6);
-      deleteCell.innerHTML = `<button class="delete-button" onclick="deleteProduct(${index})">Delete</button>`;
-  });
+  displayTable();
+  addProductCardsByCategory(category);
 }
 
 
-displayTable('All');
-function addItem(){
-const itemCode=document.getElementById("product-code").value;
-const itemName=document.getElementById("product-name").value;
-const price=document.getElementById("product-price").value;
-const discount=document.getElementById("product-discount").value;
-const category =document.getElementById("categories").value;
 
-const allProducts = JSON.parse(localStorage.getItem('allProducts')) || [];
-const newItem = { itemCode, itemName, price, discount: discount || "-" ,category};
-allProducts.push(newItem);
-localStorage.setItem('allProducts', JSON.stringify(allProducts));
-displayTable();
-addProductCardsByCategory(category);
-
-}
 function deleteProduct(index) {
   const   allProducts= JSON.parse(localStorage.getItem('allProducts')) || [];
    
